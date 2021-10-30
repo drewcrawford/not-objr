a40ca9e4cd376547aa6544ca6a1617184c87c051
Drew Crawford <drew@sealedabstract.com>
2021-10-30 15:03:11 -0500

    Rev documentation?

--- src/performselector.rs
+++ src/performselector.rs
@@ -1,3 +1,7 @@
+///Trait that provides `PerformSelector` implementations.  Autoimplelmented for `T: PerformablePointer`
+///
+/// # Stability
+/// Do not implement this trait yourself.  Instead use [objc_instance!] or [objc_class!]
 pub trait PerformsSelector  {
 
 }
